c97a9c424c8504b6a57af575dfdca6fd713a398e
Arda Hamamcıoğlu <ardham+dev@pm.me>
2026-01-04 20:51:49 +0100

    Add clean option to build script
    The build.sh script now supports a 'clean' or '--clean' argument to remove the build directory before configuring the project.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ "$1" == "clean" ] || [ "$1" == "--clean" ]; then
+    echo "Cleaning build directory..."
+    rm -rf build
+fi
+
 # Configure the project
 cmake -B build
 
